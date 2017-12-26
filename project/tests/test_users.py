c3d4ecd609d8a73368204e6bef08a5b1933c7487
Vincent Beausoleil <vincent.beausoleil@gmail.com>
2017-12-25 20:57:36 -0500

    Part 3: Flask Migrate

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -6,6 +6,8 @@ import datetime
 from project import db
 from project.api.models import User
 from project.tests.base import BaseTestCase
+from project.tests.utils import add_user
+
 
 def add_user(username, email, created_at=datetime.datetime.utcnow()):
     user = User(username=username, email=email, created_at=created_at)
@@ -66,7 +68,7 @@ class TestUserService(BaseTestCase):
             self.assertIn('Invalid payload.', data['message'])
             self.assertIn('fail', data['status'])
 
-    def test_add_user_duplicate_user(self):
+    def test_add_user_duplicate_email(self):
         """Ensure error is thrown if the email already exists."""
         with self.client:
             self.client.post(
