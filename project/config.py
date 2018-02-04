3973fa59b7d223f965098c0cbf084fc625b38918
vbeausoleil <vbeausoleil@users.noreply.github.com>
2018-02-03 23:38:35 -0500

    Update config.py

--- project/config.py
+++ project/config.py
@@ -25,6 +25,7 @@ class TestingConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     BCRYPT_LOG_ROUNDS = 4
     TOKEN_EXPIRATION_DAYS = 0
     TOKEN_EXPIRATION_SECONDS = 3
