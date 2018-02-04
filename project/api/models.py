0b058734dc647e057f1ec2f188b9818cfb37fe37
vbeausoleil <vbeausoleil@users.noreply.github.com>
2018-02-03 23:40:20 -0500

    Update models.py

--- project/api/models.py
+++ project/api/models.py
@@ -56,7 +56,7 @@ class User(db.Model):
         """
         try:
             payload = jwt.decode(
-                auth_token, current_app.config.get('SECRET_KEY'))
+                auth_token, "TEST")
             return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
